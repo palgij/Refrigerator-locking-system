1f035f1498d5a5025250e70750d3510f6582a0bc
JPalgi <palgijoel@gmail.com>
2019-05-14 22:45:50 +0300

    change app.js

--- app.js
+++ app.js
@@ -15,17 +15,6 @@ var bodyParser =   require("body-parser"),
 
 app.use(requestIp.mw());
 
-process.on('uncaughtException', function (exception) {
-  console.log(exception); // to see your exception details in the console
-  // if you are on production, maybe you can send the exception details to your
-  // email as well ?
-});
-
-process.on('unhandledRejection', (reason, p) => {
-    console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
-    // application specific logging, throwing an error, or other logic here
-});
-
 // ============ ROUTES ============
 
 var ostmineRoutes = require("./routes/ostmine"),
